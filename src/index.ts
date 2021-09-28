d3dd54005169ebf1e68045ee61700ee5abaa96a0
Gregory Denys <gdenys@pfdrive.com>
2021-09-27 21:56:34 -0400

    adjust main index export and change isolated modules flag to false

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,14 @@
-import { createBaseDimension } from './createBaseDimension'
-import { createInterstellarDimension } from './createInterstellarDimension'
-import { 
+export { createBaseDimension } from './createBaseDimension'
+export { createInterstellarDimension } from './createInterstellarDimension'
+export { 
     getStore, 
     setStore, 
     createStore 
 } from './storeRegistry'
-import { Provider } from './Provider'
-import { 
+export { Provider } from './Provider'
+export { 
     Dimension, 
     DimensionDefinitions,
     DimensionReducers, 
     StringTheoryStore 
 } from './types'
-
-export {
-    getStore,
-    setStore,
-    createStore,
-    createBaseDimension,
-    createInterstellarDimension,
-    Provider,
-    Dimension, 
-    DimensionReducers,
-    DimensionDefinitions, 
-    StringTheoryStore 
-}
